7bf110867b28125f897fd4c644e6e30398de39fa
Вячеслав Казаков <vakazakov-89@yandex.ru>
2022-06-05 19:41:23 +0300

    Test post_edit_form change

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -53,7 +53,7 @@ class PostFormsTest(TestCase):
         form_data = {
             'text': 'Отредактированный текст',
         }
-        response = self.authorized_client.get(
+        response = self.authorized_client.post(
             reverse('posts:post_edit', args=[str(self.post.id)]),
             data=form_data,
             follow=True
